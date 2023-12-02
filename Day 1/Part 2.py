98bda778ffc8f33358786a25fa38c9bc76f287d4
Darjoo <joodarrick@gmail.com>
2023-12-02 10:36:39 +0100

    Remove array usage for part 2 of day 1

--- Day 1/Part 2.py
+++ Day 1/Part 2.py
@@ -1,23 +1,23 @@
 def getNumberPart2(string: str):
-    numbers = [0] * len(string)
     all_numbers = 'one,two,three,four,five,six,seven,eight,nine'
     rep = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven': 7, 'eight':8, 'nine':9}
     p1, p2 = 0, 1
     first, second = 0, 0
 
     while p2 <= len(string):
+        number = 0
         if string[p1].isnumeric():
-            numbers[p1] = int(string[p1])
+            number = int(string[p1])
         else:  
             while p2 <= len(string) and string[p1:p2] in all_numbers:
                 p2 += 1
             if string[p1:p2-1] in rep:
-                numbers[p1] = rep[string[p1:p2-1]]
+                number = rep[string[p1:p2-1]]
 
-        if numbers[p1] != 0:
-            second = numbers[p1]
+        if number != 0:
+            second = number
             if not first:
-                first = numbers[p1]
+                first = number
 
         p1 += 1
         p2 = p1 + 1
