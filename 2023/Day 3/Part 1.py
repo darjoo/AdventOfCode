3adaa8516e69a5f3eedf98c6a69d33c9868735c3
Darjoo <joodarrick@gmail.com>
2023-12-03 15:51:11 +0100

    Refactor to remove duplicate code

--- 2023/Day 3/Part 1.py
+++ 2023/Day 3/Part 1.py
@@ -1,3 +1,11 @@
+def ReplaceWithZeros(input: list[int], start: int, end: int, num: int) -> list[int]:
+    for i in range(start, end, 1 if start < end else -1):
+        if input[i] != num:
+            break
+        input[i] = 0
+
+    return input
+
 def GetLocalParts(input: list[int], position: int) -> (list[int], int):
     parts_sum = 0
     # Replace values in list once completed
@@ -5,87 +13,56 @@ def GetLocalParts(input: list[int], position: int) -> (list[int], int):
     if position-1 >= 0:
         if input[0][position-1] > 0:
             parts_sum += input[0][position-1]
-            num = input[0][position-1]
-            
-            for i in range(position-1, 0, -1):
-                if input[0][i] != num:
-                    break
-                input[0][i] = 0
+            input[0] = ReplaceWithZeros(input[0], position-1, 0, input[0][position-1])
 
     # Check right
     if position+1 < len(input[0]):
         if input[0][position+1] > 0:
             parts_sum += input[0][position+1]
-            num = input[0][position+1]
-
-            for i in range(position+1, len(input[0])):
-                if input[0][i] != num:
-                    break
-                input[0][i] = 0
+            input[0] = ReplaceWithZeros(input[0], position+1, len(input[0]), input[0][position+1])
 
     # Check bottom
     if input[1][position] > 0:
         parts_sum += input[1][position]
-        num = input[1][position]
-
-        for i in range(position, len(input[1])):
-            if input[1][i] != num:
-                break
-            input[1][i] = 0
-
-        for i in range(position-1, 0, -1):
-            if input[1][i] != num:
-                break
-            input[1][i] = 0
+        input[1] = ReplaceWithZeros(input[1], position+1, len(input[1]), input[1][position])
+        input[1] = ReplaceWithZeros(input[1], position, 0, input[1][position])
 
     # Check bottom left
     if position-1 >= 0:
         if input[1][position-1] > 0:
             parts_sum += input[1][position-1]
-            num = input[1][position-1]
-
-            for i in range(position-1, 0, -1):
-                if input[1][i] != num:
-                    break
-                input[1][i] = 0
+            input[1] = ReplaceWithZeros(input[1], position-1, 0, input[1][position-1])
 
     # Check bottom right
     if position+1 < len(input[0]):
         if input[1][position+1] > 0:
             parts_sum += input[1][position+1]
-            num = input[1][position+1]
-
-            for i in range(position+1, len(input[1])):
-                if input[1][i] != num:
-                    break
-                input[1][i] = 0
+            input[1] = ReplaceWithZeros(input[1], position+1, len(input[1]), input[1][position+1])
 
     return input, parts_sum
     
 
-def GetParts(input: list[str]) -> int:
+def GetParts(input: list[int]) -> (list[int], int):
     part_sum = 0
-    for idx, c in enumerate(input[0]):
-        if c == -1:
+    for idx, (c0, c1) in enumerate(zip(input[0], input[1])):
+        ps = 0
+        if c0 == -1:
             input, ps = GetLocalParts(input, idx)
-            part_sum += ps
-    
-    for idx, c in enumerate(input[1]):
-        if c == -1:
+
+        if c1 == -1:
             input, ps = GetLocalParts(input[::-1], idx)
-            part_sum += ps
             input = input[::-1]
+
+        part_sum += ps
 
     return input, part_sum
 
 def BuildNumbers(input: list[str]) -> list[int]:
     numbers = []
-    p1, p2 = -1, -1
-    num = 0
+    num, p1, p2 = 0, -1, -1
     for idx, c in enumerate(input):
         if c.isnumeric():
-            num *= 10
-            num += int(c)
+            num = (num * 10) + int(c)
             if p1 == -1:
                 p1 = idx
                 p2 = idx+1
@@ -94,12 +71,8 @@ def BuildNumbers(input: list[str]) -> list[int]:
         else:
             for _ in range(p1, p2):
                 numbers.append(num)
-            if c == '.':
-                numbers.append(0)
-            else:
-                numbers.append(-1)
-            num = 0
-            p1, p2 = -1, -1
+            numbers.append(0 if c == '.' else -1)
+            num, p1, p2 = 0, -1, -1
 
     if p1 != -1:
         for _ in range(p1, p2):
@@ -115,7 +88,6 @@ for line in f.readlines():
     check_cur.append(BuildNumbers(list(line.rstrip())))
     if len(check_cur) == 2:
         check_cur, ss = GetParts(check_cur)
-        print(check_cur)
         result += ss
         check_cur.pop(0)
 
