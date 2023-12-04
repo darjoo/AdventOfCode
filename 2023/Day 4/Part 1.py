ea4be8e843247ecd390c0afed21321acd8c90cf3
Darjoo <joodarrick@gmail.com>
2023-12-04 13:17:51 +0100

    Update part 1 to also use regex boundary

--- 2023/Day 4/Part 1.py
+++ 2023/Day 4/Part 1.py
@@ -1,17 +1,15 @@
 import re
 
 f = open('input.txt', 'r')
-nums = [cards.split(':|')[1:2] for cards in f.readlines()]
+nums = [cards.split(':')[1] for cards in f.readlines()]
 f.close()
 
 result = 0
 
 for i, cur_nums in enumerate(nums):
-    win, scratched = cur_nums.split('|') 
-    winnings = {w for w in re.findall(r'\d+', win)}
-    scratched = {s for s in re.findall(r'\d+', scratched)} 
-    ww = 1 if len(winnings.intersection(scratched)) else 0
-    for x in range(1,len(winnings.intersection(scratched))):
+    length = len(re.findall(r'\b(\d+)\b(?=.*\b\1\b)', cur_nums))
+    ww = 1 if length else 0
+    for x in range(1,length):
         ww*=2
     result += ww
 
